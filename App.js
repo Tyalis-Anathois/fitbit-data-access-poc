83a26d1bfb346efe5f4c6c47574475971a2838e4
ToOcMGit <to_oc_m_git@outlook.be>
2022-05-15 00:37:02 +0200

    Added links to get started + expo info , Made color less aggressive for eyes

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
+      <Text>Follow steps from : https://reactnative.dev/docs/environment-setup</Text>
+      <Text>Expo infos : https://expo.dev/</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#555',
     alignItems: 'center',
     justifyContent: 'center',
   },
